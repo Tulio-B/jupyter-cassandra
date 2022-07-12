8e3873b7d195bc061f4f388c81b829a99f1f4311
Tulio Bustos <tuliobbustos@gmail.com>
2022-07-12 19:43:50 -0300

    Finaliza as consultas

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -1,3 +1,4 @@
 #!bin/bash
+sudo sed -i 's/^enable_materialized_views: .*$/enable_materialized_views: true/' /opt/apache-cassandra-4.0.4/conf/cassandra.yaml
 sudo ln -sf /opt/apache-cassandra-4.0.4/bin/cqlsh /usr/bin/cqlsh
 nohup sudo /opt/apache-cassandra-4.0.4/bin/cassandra -R &> /home/jovyan/cassandra.log & 
